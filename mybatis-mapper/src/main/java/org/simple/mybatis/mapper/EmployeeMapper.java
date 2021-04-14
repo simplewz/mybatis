1ec840285de9865e1c4d1588a180deb614e49d35
wangzhao <13132398914@163.com>
2021-04-15 00:12:37 +0800

    添加MyBatis传参学习代码

--- mybatis-mapper/src/main/java/org/simple/mybatis/mapper/EmployeeMapper.java
+++ mybatis-mapper/src/main/java/org/simple/mybatis/mapper/EmployeeMapper.java
@@ -4,12 +4,23 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.simple.mybatis.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeMapper {
 
     //注解和xml配置文件同时生效时，不能对同一个方法使用既使用注解又使用xml配置,mybatis框架会抛出异常
     //java.lang.IllegalArgumentException: Mapped Statements collection already contains value for org.simple.mybatis.mapper.EmployeeMapper.selectById. please check org/simple/mybatis/mapper/EmployeeMapper.xml and org/simple/mybatis/mapper/EmployeeMapper.java (best guess)
     //@Select("select * from employee where id = #{id}")
     public Employee selectById(@Param("id") String id);
+
+    //演示传入多个参数
+    public List<Employee> selectByGenderAndName(@Param("gender") String gender,@Param("name") String name);
+
+    //演示传入list集合作为参数
+    public List<Employee> selectListByIdList(@Param("IdList") List<String> IdList);
+
+    //演示传入数组参数
+    public List<Employee> selectListByIdArray(@Param("IdArray") String[] IdArray);
 
     /**
      * mybatis允许增删改操作直接定义以下类型的返回值:
--- mybatis-mapper/src/test/java/MyBatisTest.java
+++ mybatis-mapper/src/test/java/MyBatisTest.java
@@ -8,6 +8,8 @@ import org.simple.mybatis.mapper.EmployeeMapper;
 import org.simple.mybatis.util.SqlSessionFactoryUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RunWith(JUnit4.class)
@@ -45,6 +47,40 @@ public class MyBatisTest {
             /*
             //    4.4 测试删除
             System.out.println(employeeMapper.deleteById("3"));
+            */
+        }finally {
+            //5.关闭SqlSession
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testParam() throws IOException {
+        //1. 创建一个SQlSessionFactory连接工厂
+        String resource="config/mybatis-config.xml";
+        SqlSessionFactory sqlSessionFactory= SqlSessionFactoryUtil.createSqlSessionFactory(resource);
+        //2. 创建SqlSession
+        SqlSession sqlSession=sqlSessionFactory.openSession(true);
+        try{
+            //3. 根据SqlSession获取Mapper对象,该Mapper对象为动态代理对象
+            EmployeeMapper employeeMapper=sqlSession.getMapper(EmployeeMapper.class);
+            //4. 使用mapper接口的动态代理对象进行业务流程的编写
+
+
+            List<Employee> employees=employeeMapper.selectByGenderAndName("男","wangzhao");
+            System.out.println(employees);
+
+            /*
+            List<String> idList=new ArrayList<>();
+            idList.add("6cda426d-9d1b-11eb-98f3-525400d5bdb5");
+            idList.add("d25b18ad-f6ba-4077-bc71-f721673ae2b7");
+            List<Employee> employees=employeeMapper.selectListByIdList(idList);
+            System.out.println(employees);
+            */
+            /*
+            String[] idArray=new String[]{"6cda426d-9d1b-11eb-98f3-525400d5bdb5","d25b18ad-f6ba-4077-bc71-f721673ae2b7"};
+            List<Employee> employees=employeeMapper.selectListByIdArray(idArray);
+            System.out.println(employees);
             */
         }finally {
             //5.关闭SqlSession
